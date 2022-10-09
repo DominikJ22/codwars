31acd85665d9996e7fdfd83859e6f107ffa5a695
Dominik Jones <dominikjo22@gmail.com>
2022-10-09 19:34:51 -0400

    completed a code wars

---
+++ 8kyu/Reverse-string.js
@@ -0,0 +1,12 @@
+//Reversed Strings
+/*xDESCRIPTION:
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+//solution
+function solution(str){
+    return str.split('').reverse().join('');  
+  }
